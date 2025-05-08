72473d8b829a77ace9c9582a859a296478b3ca9a
CTECH <weerasinghekaushalya09@gmail.com>
2025-05-08 12:00:23 +0530

    ui created

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
-const task_input = document.getElementById('task_input');
-const addTask = document.getElementById('addTask')
+const newTask = document.getElementById('newTask');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+const filterBtns = document.querySelectorAll('.filters button');
+
+let todos =[];
+
+
+addBtn.addEventListener('click',() => {
+    const text = newTask.value.trim();
+    if(text !== ""){
+        todos.push({text, done: false});
+        newTask.value ="";
+        renderList();
+    }
+});
+
+function renderList(filter = 'all'){
+    todoList.innerHTML ="";
+     todos
+     .filter(todo => {
+        if(filter === 'done') return todo.done;
+        if(filter === 'not-done') return  !todo.done;
+    return true;
+    })
+    .forEach((todo, index) => {
+        const li = document.createElement('li');
+        li.className = todo.done ? 'done':'';
+
+        const span = document.createElement('span');
+        span.textContent = todo.text;
+        span.style.cursor = 'pointer';
+        span.onclick = () => {
+            todos[index].done = !todos[index].done;
+            renderList(currentFilter);
+        };
+        const del = document.createElement('button');
+        del.textContent = 'X';
+        del.onclick = () => {
+            todos.splice(index,1);
+            renderList(currentFilter);
+        };
+
+        li.appendChild(span);
+        li.appendChild(del);
+        todoList.appendChild(li);
+    });
+}
+
+let currentFilter = 'all';
+ filterBtns.forEach(btn => {
+    btn.addEventListener('click',() => {
+        filterBtns.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+        currentFilter = btn.dataset.filter;
+        renderList(currentFilter);
+    });
+ });
+
+ renderList();
+
